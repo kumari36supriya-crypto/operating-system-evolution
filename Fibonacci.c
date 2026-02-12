de65025b565d2116579939835df77744e5fc78c1
unknown <kumari36supriya@gmail.com>
2026-02-12 22:12:39 +0530

    both program fibonacci and reverse done

---
+++ Fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+
+    int a = 0, b = 1, c;
+
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci number: %d", fibonacci(n));
+
+    return 0;
+}
